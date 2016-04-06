64960e19459255ceafa9ca64e687edaf562e284a
Chong Jiang <sakeven@users.noreply.github.com>
2016-04-06 19:16:46 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,5 +5,6 @@ now=$(($(date +%s%N)/1000000))
 metric=load_value_test
 value=42
 host=$KAIROSDB_PORT_4242_TCP_ADDR
+echo "testing $host"
 
 echo "put $metric $now $value host=A" | nc -w 30 $host 4242
